40c6915940869b871a1a3bf52bf58495505d3c9d
Caelvanost <caelvanost@gmail.com>
2026-08-17 07:19:38 +0200

    Add SKSE save listener scaffold

---
+++ src/PCH.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <RE/Skyrim.h>
+#include <SKSE/SKSE.h>
+#include <SKSE/Logger.h>
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+#include <memory>
